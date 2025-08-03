77120bbcc6e56263b499021287c0df65eead1827
mdrifatulislam <mdrifatulislam1080@gmail.com>
2025-08-04 02:25:51 +0600

    added javascript problem 1 to 5

---
+++ Problem-04.js
@@ -0,0 +1,14 @@
+/** Problem 04 - (Delete / Store) */
+var fileName = "pdfData.jpg";
+//write your code here
+
+if (fileName[0] === "#") {
+  console.log("Store");
+} else {
+  var array = fileName.split(".");
+  if (array[1] === "pdf" || array[1] == "docx") {
+    console.log("Store");
+  } else {
+    console.log("Delete");
+  }
+}
